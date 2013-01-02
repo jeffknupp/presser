27db5926f1b0a774dc382df02f71f4aac31e2c59
Jeff Knupp <jeffrey.knupp@wellsfargo.com>
2013-01-02 12:20:04 -0500

    much faster version

--- presser.py
+++ presser.py
@@ -1,30 +1,26 @@
-from bisect import bisect_left
 from itertools import combinations
+import collections
 
 
 def load_anagrams():
+    anagrams = collections.defaultdict(list)
+    lengths = set()
     with open('anadict.txt', 'r') as file_handle:
-        anagrams = file_handle.read().split('\n')
-    return anagrams
+        for line in file_handle:
+            words = line.split()
+            anagrams[tuple(words[0])] = words[1:]
+            lengths.add(len(words[0]))
+    return (anagrams, max(lengths))
 
 
-def score_word(word):
-    return sum([scores[c] for c in word])
-
-
-def find_words(board, anagrams):
+def find_words(board, anagrams, max_length):
     board = ''.join(sorted(board))
     target_words = []
-    for word_length in range(5, len(board) + 1):
+    for word_length in range(3, max_length + 1):
         print(word_length)
         for combination in combinations(board, word_length):
-            anagram = ''.join(combination)
-            j = bisect_left(anagrams, anagram)
-            if j == len(anagram):
-                continue
-            words = anagrams[j].split()
-            if words[0] == anagram:
-                target_words.extend(words[1:])
+            if combination in anagrams:
+                target_words += anagrams[combination]
     return target_words
 
 if __name__ == "__main__":
@@ -33,7 +29,7 @@ if __name__ == "__main__":
         rack = sys.argv[1].strip()
     else:
         exit()
-    anagrams = load_anagrams()
-    target_words = set(find_words(rack, anagrams))
-    large_words = [word for word in target_words if len(word) > 5]
+    anagrams, max_length = load_anagrams()
+    target_words = set(find_words(rack, anagrams, max_length))
+    large_words = [word for word in target_words if len(word) > 14]
     print(large_words)
